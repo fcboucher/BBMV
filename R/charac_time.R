dafbbeb792b4a80c20f5dbcdfe923d4cb8bc387a
fcboucher <flofloboucher@gmail.com>
2016-10-24 12:10:45 +0200

    eigenvalue

--- R/charac_time.R
+++ R/charac_time.R
@@ -6,7 +6,7 @@ charac_time=function(Npts,model){ # X=c(Npts,bound,a,b,c,sigma)
 	if (npar==4){a=b=0 ; c=model$par$c}
 	if (npar==5){a=0 ; b=model$par$b ; c=model$par$c}
 	if (npar==6){a=model$par$a ; b=model$par$b ; c=model$par$c}
-	# build potential and stationary distribution of the trait
+	# build potential and get second largest eigenvalue
 	SEQ=seq(from=-1.5,to=1.5,length.out=Npts)
 	Vq=a*SEQ^4+b*SEQ^2+c*SEQ
 	Mat=DiffMat_forward(Vq)
