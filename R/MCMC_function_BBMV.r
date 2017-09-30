d49c9ee426751400b799b07c432d737eee7862d8
fcboucher <flofloboucher@gmail.com>
2017-09-30 15:49:27 +0200

    no need to check trait/bounds again

--- R/MCMC_function_BBMV.r
+++ R/MCMC_function_BBMV.r
@@ -6,8 +6,6 @@
 ###############################################################################
 # calculate log-likelihood over the whole tree with root value as a parameter, to be maximized
 LogLik_bounds_est_root=function(tree,trait ,dCoeff,V, x0_pos,bounds){
-if ((bounds[1]>min(trait))|(bounds[2]<max(trait))) {return(-Inf)} # bounds have to be outside the trait interval
-	else {
 Npts_tot=length(V)		
 tree_formatted=FormatTree_bounds(tree,trait,V,bounds)		
 dMat=DiffMat_backwards(V)
@@ -21,8 +19,6 @@ for (i in 1:dim(tree_formatted2$tab)[1]){
 	logFactor=logFactor+log(norm)
 }
 return(log(tree_formatted2$Pos[[tree_formatted2$tab[i,1]]][x0_pos])+logFactor) # extraction of the likelihood for a given root value (in the ML optimization version, we take max(tree_formatted2$Pos[[tree_formatted2$tab[i,1]]]))
-
-	}
 }
 
 ###############################################
