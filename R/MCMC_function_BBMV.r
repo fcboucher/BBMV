f19adf0d4305dcc4f49a9211eb95b8c0c75e0a8a
Florian Boucher <flofloboucher@gmail.com>
2018-06-05 14:01:18 +0200

    length of polygon is Npts!

--- R/MCMC_function_BBMV.r
+++ R/MCMC_function_BBMV.r
@@ -173,7 +173,7 @@ get.landscape.FPK.MCMC=function(chain,bounds,Npts=100,burnin=0.1,probs.CI=c(0.05
   if (is.null(ylim)){ylim=c(0,max(all_V))}
   par(mfrow=c(1,1))
   plot(apply(all_V,2,function(x){quantile(x,probs=c(0.5))})~seq(from=bounds[1],to=bounds[2],length.out=Npts),col=COLOR_MEDIAN,lwd=3,type='l',main=main,ylab=ylab,xlab=xlab,xlim=xlim,ylim=ylim)
-  polygon(x=c(seq(from=bounds[1],to=bounds[2],length.out=100),seq(from=bounds[2],to=bounds[1],length.out=100)),y=c(apply(all_V,2,function(x){quantile(x,probs=probs.CI[1])}),rev(apply(all_V,2,function(x){quantile(x,probs=probs.CI[2])}))),col=adjustcolor(col=COLOR_FILL,alpha.f=transparency))
+  polygon(x=c(seq(from=bounds[1],to=bounds[2],length.out=Npts),seq(from=bounds[2],to=bounds[1],length.out=Npts)),y=c(apply(all_V,2,function(x){quantile(x,probs=probs.CI[1])}),rev(apply(all_V,2,function(x){quantile(x,probs=probs.CI[2])}))),col=adjustcolor(col=COLOR_FILL,alpha.f=transparency))
 }
 
 
